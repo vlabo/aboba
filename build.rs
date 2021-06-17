37223182e042c6be489f44594a7a398b0be772e5
vlabo <vladdocan@gmail.com>
2021-06-17 22:25:11 +0300

    Fix arm build

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    #[cfg(target_arch = "x86_64")]
     cc::Build::new()
         .file("src/c/player.c")
         .flag("-I/usr/include/gstreamer-1.0")
@@ -7,6 +8,14 @@ fn main() {
         .flag("-I/usr/lib/x86_64-linux-gnu/glib-2.0/include")
         .compile("player");
 
+    #[cfg(target_arch = "aarch64")]
+    cc::Build::new()
+        .file("src/c/player.c")
+        .flag("-I/usr/include/gstreamer-1.0")
+        .flag("-I/usr/include/aarch64-linux-gnu")
+        .flag("-I/usr/include/glib-2.0")
+        .flag("-I/usr/lib/aarch64-linux-gnu/glib-2.0/include")
+        .compile("player");
 
         println!("cargo:rustc-link-lib=dylib=gstreamer-1.0");
 }
